53236fa0f7722cea03892d90956c1773f1d600ac
asadbekme <asadbekme2002@gmail.com>
2024-06-17 20:18:48 +0500

    feat: added responsive web design

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".header__mobile-menu");
 const menuCloseIcon = document.querySelector(".header__mobile-menubar-close");
 const menuBar = document.querySelector(".header__mobile-menubar");
+const menuLinks = document.querySelectorAll(".header__mobile-menubar-link");
 
 menuIcon.addEventListener("click", () => {
   menuBar.style.display = "flex";
@@ -8,4 +9,10 @@ menuIcon.addEventListener("click", () => {
 
 menuCloseIcon.addEventListener("click", () => {
   menuBar.style.display = "none";
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuBar.style.display = "none";
+  });
 });
